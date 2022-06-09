6cf1363a5088975076403cabca5b771ffcc0765c
aosaif <aosaif@hotmail.com>
2022-06-08 22:18:43 -0400

    improved styling and format for mobile

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const navLinks = document.querySelectorAll('.nav-item')
+const navItems = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
 const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
-navLinks.forEach((l) => {
+navItems.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
+
+const navLinks = document.querySelectorAll('.nav-link')
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => {
+        navLinks.forEach((m) => {
+            m.classList.remove('active')
+        })
+        l.classList.add('active')
+    }
+    )
+})
+
